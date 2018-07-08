3d760207daa51dffef1ac517034fecf10d29b3c2
Oleg Minkov <oaminkov@gmail.com>
2018-07-09 01:03:53 +0300

    Добавлена простая ф-ия Задача #8

--- backend/main.go
+++ backend/main.go
@@ -2,7 +2,16 @@ package main
 
 import "fmt"
 
+// Sum суммирует два целых числа
+func Sum(a, b int) int {
+	var c = a + b
+	return c
+}
+
 func main() {
 	fmt.Println("Hello, World!")
 	fmt.Printf("Привет, %s", "Мир")
+
+	var c = Sum(7, 8)
+	fmt.Printf("\nСумма = %d", c)
 }
