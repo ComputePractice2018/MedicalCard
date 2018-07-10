df892f096a1f1e3a7c9d9bb9af0d51c3dfd50607
Oleg Minkov <oaminkov@gmail.com>
2018-07-10 23:09:03 +0300

    Сделан REST API Задача #18

--- backend/server/handlers.go
+++ backend/server/handlers.go
@@ -5,26 +5,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/ComputePractice2018/medicalcard/backend/data"
+	"github.com/gorilla/mux"
 )
 
 // GetAppointments обрабатывает запросы на получение списка записей пациентов
 func GetAppointments(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Request method: %s", r.Method)
-	binaryData, err := json.Marshal(data.AppointmentList)
-	if err != nil {
-		message := fmt.Sprintf("JSON marshaling error: %v", err)
-		http.Error(w, message, http.StatusInternalServerError)
-		return
-	}
 	w.Header().Add("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	_, err = w.Write(binaryData)
+	err := json.NewEncoder(w).Encode(data.GetAppointments())
 	if err != nil {
-		message := fmt.Sprintf("Handler write error: %v", err)
+		message := fmt.Sprintf("Unable to encode data: %v", err)
 		http.Error(w, message, http.StatusInternalServerError)
 		log.Println(message)
+		return
 	}
 }
 
@@ -38,14 +34,59 @@ func AddAppointment(w http.ResponseWriter, r *http.Request) {
 		log.Println(message)
 		return
 	}
-	log.Printf("%+v", appointment)
+	id := data.AddAppointment(appointment)
+	w.Header().Add("Location", r.URL.String()+"/"+strconv.Itoa(id))
 	w.WriteHeader(http.StatusCreated)
 }
 
+// EditAppointment ообрабатывает PUT запрос
 func EditAppointment(w http.ResponseWriter, r *http.Request) {
+	var appointment data.Appointment
+	err := json.NewDecoder(r.Body).Decode(&appointment)
+	if err != nil {
+		message := fmt.Sprintf("Unable to decode PUT data: %v", err)
+		http.Error(w, message, http.StatusUnsupportedMediaType)
+		log.Println(message)
+		return
+	}
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		message := fmt.Sprintf("Incorrect ID: %v", err)
+		http.Error(w, message, http.StatusBadRequest)
+		log.Println(message)
+		return
+	}
 
+	err = data.EditAppointment(appointment, id)
+	if err != nil {
+		message := fmt.Sprintf("Incorrect ID: %v", err)
+		http.Error(w, message, http.StatusBadRequest)
+		log.Println(message)
+		return
+	}
+	w.Header().Add("Location", r.URL.String())
+	w.WriteHeader(http.StatusAccepted)
 }
 
+// DeleteAppointment обрабатывает DELETE запрос
 func DeleteAppointment(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		message := fmt.Sprintf("Incorrect ID: %v", err)
+		http.Error(w, message, http.StatusBadRequest)
+		log.Println(message)
+		return
+	}
 
+	err = data.RemoveAppointment(id)
+	if err != nil {
+		message := fmt.Sprintf("Incorrect ID: %v", err)
+		http.Error(w, message, http.StatusBadRequest)
+		log.Println(message)
+		return
+	}
+	w.Header().Add("Location", r.URL.String())
+	w.WriteHeader(http.StatusNoContent)
 }
